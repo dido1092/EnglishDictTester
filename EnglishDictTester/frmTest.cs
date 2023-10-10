5c7338ad22c97161ff64a9de49f185d2494ac6f7
Dido <deyangospodinov1092@abv.bg>
2023-10-10 18:48:24 +0300

    Form Test Focus TextBoxTranslateWord

--- EnglishDictTester/frmTest.cs
+++ EnglishDictTester/frmTest.cs
@@ -289,6 +289,7 @@ namespace EnglishDictTester
                     textBoxTranslateWord.Text = "";
 
                     Pronounce();
+                    textBoxTranslateWord.Focus();
                 }
             }
         }
