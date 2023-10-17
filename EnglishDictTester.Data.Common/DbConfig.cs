10d0bece6b8cf3fcc78cae2d0e62d1fcbe7c19a2
kasis <kasis@KASIS>
2023-10-17 17:38:21 +0300

    Form Test Fix problem with button "Next"

--- EnglishDictTester.Data.Common/DbConfig.cs
+++ EnglishDictTester.Data.Common/DbConfig.cs
@@ -3,6 +3,6 @@
     public class DbConfig
     {
         public const string ConnectionString =
-         @"Server=DESKTOP-6M29KPQ\SQLEXPRESS;AttachDbFilename=C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\EnglishDictTester.mdf;Database=EnglishDictTester;Integrated Security=True;Encrypt=False;MultipleActiveResultSets=true;";
+         @"Server=KASIS\SQLEXPRESS;AttachDbFilename=C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\EnglishDictTester.mdf;Database=EnglishDictTester;Integrated Security=True;Encrypt=False;MultipleActiveResultSets=true;";
     }
 }
--- EnglishDictTester/frmTest.cs
+++ EnglishDictTester/frmTest.cs
@@ -186,7 +186,8 @@ namespace EnglishDictTester
         {
             int numberOfIncorrectWords = 0;
 
-            if (textBoxTranslateWord.Text != "" && correctAnswer < numberOfWords)
+            //if (textBoxTranslateWord.Text != "" && correctAnswer < numberOfWords)
+            if (textBoxTranslateWord.Text != "")
             {
                 if (isButtonLoadClicked || isButtonLoadSelectedIncorrectWordsClicked)
                 {
