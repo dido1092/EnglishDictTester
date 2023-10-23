9313812bb7d5ce2504f738b73154edf8f2878240
dido1092 <deyangospodinov1092@abv.bg>
2023-10-23 22:20:25 +0300

    Update form Test

--- EnglishDictTester.Data.Common/DbConfig.cs
+++ EnglishDictTester.Data.Common/DbConfig.cs
@@ -4,7 +4,8 @@
     {
         private static string name = Environment.MachineName.ToUpper().ToString();
         public static string ConnectionString =
-         @"Server=KASIS\SQLEXPRESS;AttachDbFilename=C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\EnglishDictTester.mdf;Database=EnglishDictTester;Integrated Security=True;Encrypt=False;MultipleActiveResultSets=true;";
+         //@"Server=KASIS\SQLEXPRESS;AttachDbFilename=C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\EnglishDictTester.mdf;Database=EnglishDictTester;Integrated Security=True;Encrypt=False;MultipleActiveResultSets=true;";
+         @"Server=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\EnglishDictTester.mdf;Database=EnglishDictTester;Integrated Security=True;Encrypt=False;MultipleActiveResultSets=true;";
          //@"Server=" + name + @"\SQLEXPRESS;AttachDbFilename=C:\Program Files\BulgarianEnglishDictTester\BulgarianEnglishDictTester.mdfDatabase=BulgarianEnglishDictTester;Integrated Security=True;Encrypt=False;MultipleActiveResultSets=true;";
          //@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Program Files\BulgarianEnglishDictTester\BulgarianEnglishDictTester.mdf;Database=BulgarianEnglishDictTester;Integrated Security=True;Encrypt=False;MultipleActiveResultSets=true;";
          //@"Data Source=C:\Program Files\BulgarianEnglishDictTester\EnglishDictTester.mdf;Database=EnglishDictTester;Integrated Security=True;Encrypt=False;MultipleActiveResultSets=true;";
